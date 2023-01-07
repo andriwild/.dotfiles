0fc7d69d4cac1ffc580e64efcbb2357eade147df
Andri Wild <andri.wild@students.fhnw.ch>
2023-01-07 09:58:10 +0100

    add packages and git resources

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
 echo "Hello from install script"
+
+pacman -S bluez bluez-utils blueberry \
+		zip unzip git curl tree wget \
+		npm gcc \
+		neovim \
+		chromium \
+		
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\\n ~/.local/share/nvim/site/pack/packer/start/packer.nvim	
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 17.0.5-librca
