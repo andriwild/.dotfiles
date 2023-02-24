928a79270f07e9f02d4b6129cccfcf1ee6376b83
Andri Wild <andri.wild@students.fhnw.ch>
2023-02-24 11:34:58 +0100

    add tmux-sessionzer scipt

---
+++ .local/bin/tmux-sessionizer.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+# inspired by theprimeagen 
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+
+directories=(
+  $(find ~/fhnw/6-Semester/ -mindepth 1 -maxdepth 3 -type d)
+) 
+selected=$(echo $directories | tr " " "\n" | fzf)
+
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _) # select the last path element 
+tmux_running=$(ps -e | grep tmux)
+
+# create new session if it a session with this name does not exist
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+if [[ -z $TMUX ]]; then  
+  tmux attach -t $selected_name
+else 
+  tmux switch-client -t $selected_name
+fi
