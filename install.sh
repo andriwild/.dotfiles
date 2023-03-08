bdb5c4f93c29a453cc1889d326be3fc090c8d1f6
Andri Wild <andri.wild@students.fhnw.ch>
2023-03-08 19:52:46 +0100

    add several packages

--- install.sh
+++ install.sh
@@ -2,30 +2,23 @@
 
 echo "Install script... "
 
-if [ "$(id -u)" != "0" ]; then
-   echo "Dieses Skript muss als Root ausgeführt werden." 1>&2
-   exit 1
-fi
+sudo pacman -Syyu
 
-pacman -Syyu
+sudo pacman -S --noconfirm yay
+sudo yay -Sua
 
-read -p "Ist yay bereits installiert? [y/n] " answer
-
-if [ "$answer" == "${answer#[Nn]}" ] ;then
-    pacman -S --noconfirm yay
-    yay -Sua
-fi
-
-pacman -S --noconfirm bluez bluez-utils blueberry \
-		zip unzip git curl tree wget htop tldr sshfs \
-		npm gcc \
-		neovim rofi nemo alacritty \
-		xorg-server-xephyr alsa-utils pavucontrol upower brightnessctl i3lock \
-		arc-icon-theme \
-		chromium qutebrowser firefox \
-		thunderbird owncloud-client flameshot onlyoffice-desktopeditors \
+sudo pacman -S --noconfirm bluez bluez-utils blueberry cifs-utils \
+		zip unzip git curl tree wget htop tldr sshfs gcc g++ \
+		make cmake gmp ncurses realpath xz-utils \
+		npm fzf \
+		neovim rofi nemo alacritty code tmux \
+		xorg-server-xephyr alsa-utils pavucontrol upower brightnessctl i3lock xclip \
+		arc-icon-theme arandr \
+		hamster-time-tracker \
+		chromium qutebrowser firefox notion \
+		thunderbird owncloud-client flameshot onlyoffice-desktopeditors okular texlive-most \
 		
-yay -S awmtt
+sudo yay -S awmtt
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
