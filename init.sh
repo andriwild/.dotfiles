8cc94f68db3b67f6a42b7d0c0964df4facbc15e1
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-08 18:51:24 +0200

    add numlockx

--- init.sh
+++ init.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-syndaemon -i 1.0 -t -K -R -d
+# map caps-lock to esc
 setxkbmap -option "ctrl:nocaps"
-xcape -e 'Super_L=Alt_L|F1;Control_L=Escape'
+
+# make sure process is not alerady running
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run "syndaemon -i 1.0 -t -K -R -d"
+run "xcape -e 'Super_L=Alt_L|F1;Control_L=Escape'"
+run "numlockx on"
+run "nm-applet"
+run "xfce4-power-manager"
+
